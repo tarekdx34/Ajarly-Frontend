98aa55a91fba5a57cae4da02689a05a2eca61e51
Tarek Mohamed <tarekdx3@gmail.com>
2025-11-26 21:29:48 +0200

    Columns Fix

--- src/components/pages/home/HeroSection.tsx
+++ src/components/pages/home/HeroSection.tsx
@@ -25,7 +25,7 @@ export function HeroSection({
       {" "}
       <div className="absolute inset-0">
         <img
-          src="src\assets\jasper-boer-CvpWxeAEUOE-unsplash.jpg"
+          src="src/assets/jasper-boer-CvpWxeAEUOE-unsplash.jpg"
           alt="Hero Background"
           className="w-full h-full object-cover"
         />
